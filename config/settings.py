ac8a1b22a0f8a263bd330ac58bb7c443292357c1
Saidanasxon <saydullayevsaidanasxon@gmail.com>
2024-12-06 19:51:58 +0500

    second commit

--- config/settings.py
+++ config/settings.py
@@ -119,26 +119,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # ============================DATABASE SQLITE============================
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # ============================DATABASE POSTGRESQL============================
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'dorixona',
-#         'USER': 'postgres',
-#         'PASSWORD': 'dorixona28',
-#         # 'HOST': 'localhost',
-#         'HOST': 'bu yerga IP joylanadi',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'landing',
+        'USER': 'root',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        # 'HOST': 'bu yerga IP joylanadi',
+        'PORT': '',
+    }
+}
 
 
 
